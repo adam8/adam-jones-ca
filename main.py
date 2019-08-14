93e9673a5a4150558428bed7c265e67c982cfa62
Adam Skye Jones <adamjones.ca@gmail.com>
2019-08-14 20:40:02 -0230

    OMG, semi-colon

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('Hello 123')
 
 class PostUpdate(webapp2.RequestHandler):
-    def get(self);
+    def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('This should be a post (I mean GET) from browser')
 
