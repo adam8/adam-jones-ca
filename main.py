ba5d4691c6ea96630a107c4aea046fbff98506b2
Adam Skye Jones <adamjones.ca@gmail.com>
2019-08-14 21:28:43 -0230

    add watch request to gmail topic subscription

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ class PostUpdate(webapp2.RequestHandler):
         self.response.write('success POST, i think from gmail')
 
 class Watch(webapp2.RequestHandler);
-    def post(self):
-      
+    def get(self):
+      request = {
+        'labelIds': ['INBOX'],
+        'topicName': 'projects/adam-jones-ca/topics/adam-jones-ca-email-post'
+      }
+    gmail.users().watch(userId='adam@adamjones.ca', body=request).execute()
 
 app = webapp2.WSGIApplication([
     ('/post123', PostUpdate),
+    ('/watch', Watch),
     ('/', MainPage)
 ], debug=True)
