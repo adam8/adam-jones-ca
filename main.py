005e9a2267ae4ac5b058b686f1a3929bbabe57b9
Adam Skye Jones <adamjones.ca@gmail.com>
2019-08-14 20:34:08 -0230

    this still work?

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello again from my phone, World!')
 
+class PostUpdate(webapp2.RequestHandler):
+    def post(self);
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('This should be a post from gmail')
 
 app = webapp2.WSGIApplication([
+    ('post', postUpdate),
     ('/', MainPage),
 ], debug=True)
